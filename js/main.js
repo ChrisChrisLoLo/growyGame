ae6342620f8294fc4563e2d2308ce7bcc97c1913
ChrisChrisLoLo <sporewoh@gmail.com>
2018-01-15 22:07:35 -0700

    Added basis for crop planting

--- js/main.js
+++ js/main.js
@@ -3,12 +3,38 @@ var globalVal = {
 	currentTool: "inspect",
 }
 
+function crop(name,growTime,buyPrice,sellPrice){
+    this.name = name;
+    this.growTime = growTime;
+    this.buyPrice = buyPrice;
+    this.sellPrice = sellPrice;
+}
+
+var cropList = [new crop("grass",10,2,3)];
+
 function selTool(toolID){
 	globalVal.currentTool=toolID;
 	console.log(globalVal.currentTool);
 }
+function usePlot(plotID){
+    console.log("usePlotCall");
+    switch(globalVal.currentTool){
+        case "inspect":
+            break;
+        case "harvest":
+            break;
+        case "destroy":
+            break;
+        case "pGrass":
+            console.log("pGrass call");
+            plant("grass",plotID);
+            break;
+    }
+}
 
-function usePlot(iGrid){
-	console.log(iGrid);
-
+function plant(cropName,plotID){
+    document.getElementById(plotID).style.backgroundColor = "Green";
+    initTime = new Date();
+    console.log(initTime.getTime());
+    //document.getElementByID(plotID).setAttribute("timestamp",)
 }
